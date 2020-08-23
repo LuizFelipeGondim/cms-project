f9c5648bac227e10558eedcf81dc3b6268e7be61
LuizFelipeGondim <luizfelipegondimaraujo@gmail.com>
2020-08-23 00:08:57 -0300

    system of scroll with jquery added

--- cms/index.php
+++ cms/index.php
@@ -20,15 +20,15 @@
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarsExample07">
-                    <ul class="navbar-nav mr-auto">
+                    <ul id="menu-principal" class="navbar-nav mr-auto">
                         <li class="nav-item active">
-                            <a class="nav-link" href="#">Editar sobre <span class="sr-only">(current)</span></a>
+                            <a class="nav-link" href="#" ref_sys="sobre">Editar sobre <span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Cadastrar equipe</a>
+                            <a class="nav-link" href="#" ref_sys="cadastrar_equipe">Cadastrar equipe</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Gerenciar equipe</a>
+                            <a class="nav-link" href="#" ref_sys="gerenciar_equipe">Gerenciar equipe</a>
                         </li>
                     </ul>
                     <ul class="nav navbar-nav navbar-right">
@@ -67,19 +67,19 @@
                 <div class="row">
                     <div class="col-md-3">
                         <div class="list-group">
-                            <a href="#" class="list-group-item color-white bg-dark">
-                                <!-- colocar ícone --> Home
+                            <a href="#" class="list-group-item color-white bg-dark" ref_sys="sobre">
+                                <!-- colocar ícone --> Sobre
                             </a>
-                            <a href="" class="list-group-item color-black">
-                                Sobre
+                            <a href="" class="list-group-item" ref_sys="cadastrar_equipe">
+                                Cadastrar equipe
                             </a>
-                            <a href="" class="list-group-item color-black">
-                                Equipe
+                            <a href="" class="list-group-item" ref_sys="gerenciar_equipe">
+                                Gerenciar equipe
                             </a>
                         </div>
                     </div>
                     <div class="col-md-9">
-                        <div class="card" style="width: 18rem;">
+                        <div class="card" style="width: 18rem;" id="sobre_section">
                             <div class="card-header bg-dark color-white">
                                 Sobre
                             </div>
@@ -96,7 +96,7 @@
                             </ul>
                         </div>
 
-                        <div class="card" style="width: 18rem;">
+                        <div class="card" style="width: 18rem;" id="cadastrar_equipe_section">
                             <div class="card-header bg-dark color-white">
                                 Cadastrar Equipe
                             </div>
@@ -117,9 +117,9 @@
                             </ul>
                         </div>
 
-                        <div class="card" style="width: 18rem;">
+                        <div class="card" style="width: 18rem;" id="gerenciar_equipe_section">
                             <div class="card-header bg-dark color-white">
-                                Membros da equipe
+                                Gerenciar equipe
                             </div>
                             <ul class="list-group list-group-flush">
                                 <li class="list-group-item">
@@ -129,21 +129,17 @@
                                                 <tr>
                                                     <th scope="col">ID</th>
                                                     <th scope="col">Nome do membro</th>
+                                                    <th scope="col">#</th>
                                                 </tr>
                                             </thead>
                                             <tbody>
-                                                <tr>
-                                                    <th scope="row">1</th>
-                                                    <td>Mark</td>
-                                                </tr>
-                                                <tr>
-                                                    <th scope="row">2</th>
-                                                    <td>Jacob</td>
-                                                </tr>
-                                                <tr>
-                                                    <th scope="row">3</th>
-                                                    <td>Larry</td>
-                                                </tr>
+                                                <?php for($i = 0; $i < 5; $i++){ ?>
+                                                    <tr>
+                                                        <th scope="row">1</th>
+                                                        <td>Mark</td>
+                                                        <td><button type="button" class="btn btn-danger">Excluir</button></td>
+                                                    </tr>
+                                                <?php } ?>
                                             </tbody>
                                         </table>
                                     </div>
@@ -155,8 +151,37 @@
             </div>
         </section>
 
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+        <script type="text/javascript">
+
+            $(function(){
+
+                scrollItem();
+
+                clickMenu();
+
+                function clickMenu(){
+                    $('#menu-principal a, .list-group a').click(function(){
+                        $('.list-group a').removeClass('bg-dark').removeClass('color-white');
+                        $('#menu-principal a').parent().removeClass('active');
+                        $('#menu-principal a[ref_sys='+$(this).attr('ref_sys')+']').parent().addClass('active');
+                        $('.list-group a[ref_sys='+$(this).attr('ref_sys')+']').addClass('bg-dark').addClass('color-white');
+
+                        return false;
+                    })
+                }
+
+                function scrollItem(){
+                    $('#menu-principal a, .list-group a').click(function(){
+                        var ref = '#'+$(this).attr('ref_sys')+'_section';
+                        var offset = $(ref).offset().top;
+                        $('html,body').animate({'scrollTop':offset});
+                    });
+                }
+
+            })
+        </script>
     </body>
 </html>
