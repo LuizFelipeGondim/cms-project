b42253a39aa1d97acd5d4f5dd93aadec0d848cab
LuizFelipeGondim <luizfelipegondimaraujo@gmail.com>
2020-08-22 21:12:27 -0300

    edition panel created

--- cms/index.php
+++ cms/index.php
@@ -8,7 +8,6 @@
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
         <link rel="stylesheet" href="css/style.css">
-        <link rel="stylesheet" href="css/navbar.css">
         <title>CMS</title>
     </head>
 
@@ -34,7 +33,7 @@
                     </ul>
                     <ul class="nav navbar-nav navbar-right">
                         <li>
-                            <a href="?sair">Sair</a> <!-- colocar ícone -->
+                            <a href="?sair" class="color-white">Sair</a> <!-- colocar ícone -->
                         </li>
                     </ul>
                 </div>
@@ -52,6 +51,109 @@
                 </div>
             </div>
         </header>
+
+        <section class="bread">
+            <div class="container">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item"><a href="#"></a>ddsg</li>
+                    <li class="breadcrumb-item active">dddd</li>
+                </ol>
+            </div>
+        </section>
+
+        <section class="principal">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="list-group">
+                            <a href="#" class="list-group-item color-white bg-dark">
+                                <!-- colocar ícone --> Home
+                            </a>
+                            <a href="" class="list-group-item color-black">
+                                Sobre
+                            </a>
+                            <a href="" class="list-group-item color-black">
+                                Equipe
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-9">
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-header bg-dark color-white">
+                                Sobre
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">
+                                    <form method="post" class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="textarea">Código HTML: </label>
+                                            <textarea name="" id="textarea" class="form-control"></textarea>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary">Enviar</button>
+                                    </form>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-header bg-dark color-white">
+                                Cadastrar Equipe
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">
+                                    <form method="post" class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="nome-membro">Nome do membro</label>
+                                            <input type="text" class="form-control" id="nome-membro">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="textarea-desc">Descrição da equipe: </label>
+                                            <textarea name="" id="textarea-desc" class="form-control"></textarea>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary">Enviar</button>
+                                    </form>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-header bg-dark color-white">
+                                Membros da equipe
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">
+                                    <div class="bd-example">
+                                        <table class="table">
+                                            <thead>
+                                                <tr>
+                                                    <th scope="col">ID</th>
+                                                    <th scope="col">Nome do membro</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <th scope="row">1</th>
+                                                    <td>Mark</td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">2</th>
+                                                    <td>Jacob</td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">3</th>
+                                                    <td>Larry</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
 
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
